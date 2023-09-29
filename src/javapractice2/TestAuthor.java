c860cc437a02202eec13b42e78df016cbb550cf1
Типок <misha.myatishkin@gmail.com>
2023-09-30 00:11:54 +0300

    Вторая

---
+++ src/javapractice2/TestAuthor.java
@@ -0,0 +1,7 @@
+package javapractice2;
+public class TestAuthor {
+    public static void main(String [] args){
+        Author author = new Author("Artur","calexicool@ya.ru",'M');
+        System.out.println(author.toString());
+    }
+}
